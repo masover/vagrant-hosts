9d08db9708db66d5bfd992fd10c6b075604ed665
Jan Brauer <jan@jimdo.com>
2012-04-05 13:23:35 +0200

    Don't create entries on :start, because :up also calls start

--- lib/vagrant-hosts.rb
+++ lib/vagrant-hosts.rb
@@ -3,10 +3,10 @@ require 'vagrant-hosts/plugin'
 
 Vagrant.config_keys.register(:hosts) {VagrantHosts::HostsConfig}
 
-# TODO: find out if this needs to be in more chains
-[:start, :up, :reload, :resume].each do |each|
+[:up, :reload, :resume].each do |each|
   Vagrant.actions[each].use VagrantHosts::HostsSetupMiddleware
 end
+
 [:destroy, :suspend].each do |each|
   Vagrant.actions[each].use VagrantHosts::HostsTeardownMiddleware
 end
